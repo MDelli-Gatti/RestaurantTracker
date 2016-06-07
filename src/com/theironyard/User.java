58e3dae1028f28389a0702de234fb7ae83be7d68
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-07 12:01:44 -0400

    submit, view, delete

--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -1,11 +1,15 @@
 package com.theironyard;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+
 /**
  * Created by michaeldelli-gatti on 6/7/16.
  */
 public class User {
     String name;
     String password;
+    AbstractList<Restaurant> restaurants = new ArrayList<>();
 
     public User(String name, String password) {
         this.name = name;
